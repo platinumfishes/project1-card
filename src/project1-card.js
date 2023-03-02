760ffe47d13b482b2dc8abe7048ed1c61c015b93
Platinumfish <mulerotmglel@gmail.com>
2023-03-02 15:43:57 -0500

    sus

--- src/project1-card.js
+++ src/project1-card.js
@@ -1,4 +1,9 @@
 import { LitElement, html, css } from 'lit';
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
+import "@prettywebcomponents/band-card/src/band-card.js";
+import "@prettywebcomponents/videogame-card2/src/videogame-card.js";
+
+
 
 const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
 
@@ -61,30 +66,7 @@ export class Project1Card extends LitElement {
 
   render() {
     return html`
-      <main>
-        <div class="logo"><img alt="open-wc logo" src=${logo} /></div>
-        <h1>${this.title}</h1>
 
-        <p>Edit <code>src/Project1Card.js</code> and save to reload.</p>
-        <a
-          class="app-link"
-          href="https://open-wc.org/guides/developing-components/code-examples/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Code examples
-        </a>
-      </main>
-
-      <p class="app-footer">
-        🚽 Made with love by
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://github.com/open-wc"
-          >open-wc</a
-        >.
-      </p>
     `;
   }
 }
